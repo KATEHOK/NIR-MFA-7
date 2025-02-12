833ed72b73ed4cc2c56fef0f78e98523ae549b1f
Katehok <a.timin2003@mail.ru>
2025-02-12 05:49:30 +0300

    Updated server: hello-api

--- server/app/database.py
+++ server/app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 
 # Конструирует ссылку для подключения к бд
@@ -14,15 +14,17 @@ def get_db_url():
         POSTGRES_PASSWORD = file.read().strip()
     return f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
+class Base(DeclarativeBase):
+    pass
+
 DATABASE_URL = get_db_url()
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(url=DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
---
+++ server/app/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime 
+from typing import Annotated
+from sqlalchemy import ForeignKey, func, text
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from .database import Base
+
+int_pk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+bool_default_false = Annotated[bool, mapped_column(nullable=False, server_default=False)]
+str_default_free = Annotated[str, mapped_column(nullable=False, server_default="")]
+datetime_default_now = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"), nullable=False)]
+datetime_default_update_now = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"), nullable=False)]
+
+class UserOrm(Base):
+    __tablename__ = 'user'
+    id: Mapped[int_pk]
+    is_active: Mapped[bool_default_false]
+    unsuccessful_logins: Mapped[int] = mapped_column(nullable=False, server_default=0)
+    successful_logins: Mapped[str_default_free]
+    key: Mapped[str_default_free]
+    otp: Mapped[str_default_free]
+    def __repr__(self):
+        return f"<User(id='{self.id}', is_active='{self.is_active}')>"
+    
+class JWTOrm(Base):
+    __tablename__ = "jwt"
+    id: Mapped[int_pk]
+    id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User")
+    is_authorized: Mapped[bool_default_false]
+    revoked: Mapped[bool_default_false]
+    issued_at: Mapped[datetime] = mapped_column(nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(nullable=False)
+    created_at: Mapped[datetime_default_now]
+    updated_at: Mapped[datetime_default_update_now]
